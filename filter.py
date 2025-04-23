9d823f2920abdb122e85338fd9b9a8ed3743c785
Willow-Zero <zmckinnis4@gmail.com>
2025-04-23 14:36:43 -0400

    refining filtering

--- filter.py
+++ filter.py
@@ -1,41 +1,66 @@
 
 ingredients_collection = db.ingredients
 
-def filter(sortBy,timeCook,ready,ingredientsUsed,haveSome):
+def filter(sortBy,timeCook,totalTime,prepTime,ready,ingredientsUsed,haveSome):
     ingredientsList = ingredients_collection.get()
     recipeList = recipe_collection.get()
     potentialRecipeIDs = []
     potRecIngredientRatio = {} # structure is _id:{"total":0,"present":0,"ratio":0}
 
-    # TODO add filtering for specific ingredients, cook time, abd favorites
+    # DONE add filtering for *specific ingredients*, cook time
 
     # TOOO add price finding feature
-    if (ingredientsUsed == []):
-
-
 
     # DONE mod filter to inclode 0% recipes
-    for recipe in recipeList:
+    for idnum,recipe in recipeList.items:
         ingredientNames = []
         for ingredient in recipe["ingredients"]:
             ingredientNames.append(recipe["ingredients"]["name"])
         if (haveSome == False):
-                potentialRecipeIDs.append(recipe["_id"])
-                potRecIngredientRatio.append(recipe["_id"]:{"total":ingredientNames.size,"present":0,"ratio":0})
+                potentialRecipeIDs.append(idnum)
+                potRecIngredientRatio.append(idnum:{"total":ingredientNames.size,"present":0,"ratio":0})
         for ingredient in ingredientsList:
             if ingredient["name"] in ingredientNames:
-                if recipe["_id"] not in potentialRecipeIDs:
-                    potentialRecipeIDs.append(recipe["_id"])
-                    potRecIngredientRatio.append(recipe["_id"]:{"total":ingredientNames.size,"present":1,"ratio":0})
-                    potRecIngredientRatio[recipe["_id"]]["ratio"] = potRecIngredientRatio[recipe["_id"]]["present"]/potRecIngredientRatio[recipe["_id"]]["total"]
+                if idnum not in potentialRecipeIDs:
+                    potentialRecipeIDs.append(idnum)
+                    potRecIngredientRatio.append(idnum:{"total":ingredientNames.size,"present":1,"ratio":0})
+                    potRecIngredientRatio[idnum]["ratio"] = potRecIngredientRatio[idnum]["present"]/potRecIngredientRatio[idnum]["total"]
                 else:
-                    potRecIngredientRatio[recipe["_id"]]["present"] = potRecIngredientRatio[recipe["_id"]]["present"]+1
-                    potRecIngredientRatio[recipe["_id"]]["ratio"] = potRecIngredientRatio[recipe["_id"]]["present"]/potRecIngredientRatio[recipe["_id"]]["total"]
+                    potRecIngredientRatio[idnum]["present"] = potRecIngredientRatio[idnum]["present"]+1
+                    potRecIngredientRatio[idnum]["ratio"] = potRecIngredientRatio[idnum]["present"]/potRecIngredientRatio[idnum]["total"]
+        if (ingredientsUsed != []):
+            allow = False
+            for ingredient in ingredientsUsed:
+                if ingredient in ingredientNames:
+                    allow = True
+            if (allow == False):
+                potentialRecipeIDs.remove(idnum)
+        if (timeCook>0):
+            if (recipe["cookTime"]>timeCook)
+                potentialRecipeIDs.remove(idnum)
+        if (totalTime>0):
+            if (recipe["cookTime"]>totalTime)
+                potentialRecipeIDs.remove(idnum)
+        if (prepTime>0):
+            if (recipe["cookTime"]>prepTime)
+                potentialRecipeIDs.remove(idnum)
+
+
+    # potentialRecipeIDs now full of all recipes with at least one present ingredient, and potRecIngredientRatio is a dict of dicts in correct format
+    def sortAlpha(n):
+        return recipeList[n]["name"]
+    def sortPercent(n):
+        return potRecIngredientRatio[n]["ratio"]
+    def sortPrice(n):
 
 
 
-
-    # potentialRecipeIDs now full of all recipes with at least one present ingredient, and potRecIngredientsRatio is a dict of dicts in correct format
+    if (sortBy == "alpha"):
+        potentialRecipeIDs.sort(key=sortAlpha)
+    else if (sortBy = "price"):
+        potentialRecipeIDs.sort(key=sortPrice)
+    else:
+        potentialRecipeIDs.sort(key=sortPercent)
 
     # TODO sort dict
     # TODO: sort by price, alphabetical, percent ingredients
