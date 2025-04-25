16313146ad01e4ff195c958507925f351bd548f6
mxa5251 <mxa5251@nyu.edu>
2025-04-25 11:35:14 -0400

    moved filter logic

--- recommender_api/app/app.py
+++ recommender_api/app/app.py
@@ -10,6 +10,7 @@ from app.db import (
     update_user_ingredients
 )
 from app.models import RecipeFilterParams
+from app.recommend import filter
 
 app = Flask(__name__, static_folder='static')
 
@@ -115,75 +116,3 @@ def create_app():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
-
-
-from app.db import recipe_collection, ingredients_collection, get_recipes, get_ingredients
-from app.models import RecipeFilterParams
-
-def filter(sortBy,timeCook,totalTime,prepTime,ready,ingredientsUsed,haveSome):
-    ingredientsList = ingredients_collection.get()
-    recipeList = recipe_collection.get()
-    potentialRecipeIDs = []
-    potRecIngredientRatio = {} # structure is _id:{"total":0,"present":0,"ratio":0}
-
-    # DONE add filtering for *specific ingredients*, cook time
-
-    # TOOO add price finding feature
-
-    # DONE mod filter to inclode 0% recipes
-    for idnum,recipe in recipeList.items:
-        ingredientNames = []
-        for ingredient in recipe["ingredients"]:
-            ingredientNames.append(recipe["ingredients"]["name"])
-        if (haveSome == False):
-                potentialRecipeIDs.append(idnum)
-                potRecIngredientRatio.append(idnum:{"total":ingredientNames.size,"present":0,"ratio":0})
-        for ingredient in ingredientsList:
-            if ingredient["name"] in ingredientNames:
-                if idnum not in potentialRecipeIDs:
-                    potentialRecipeIDs.append(idnum)
-                    potRecIngredientRatio.append(idnum:{"total":ingredientNames.size,"present":1,"ratio":0})
-                    potRecIngredientRatio[idnum]["ratio"] = potRecIngredientRatio[idnum]["present"]/potRecIngredientRatio[idnum]["total"]
-                else:
-                    potRecIngredientRatio[idnum]["present"] = potRecIngredientRatio[idnum]["present"]+1
-                    potRecIngredientRatio[idnum]["ratio"] = potRecIngredientRatio[idnum]["present"]/potRecIngredientRatio[idnum]["total"]
-        if (ingredientsUsed != []):
-            allow = False
-            for ingredient in ingredientsUsed:
-                if ingredient in ingredientNames:
-                    allow = True
-            if (allow == False):
-                potentialRecipeIDs.remove(idnum)
-        if (timeCook>0):
-            if (recipe["cookTime"]>timeCook):
-                potentialRecipeIDs.remove(idnum)
-        if (totalTime>0):
-            if (recipe["cookTime"]>totalTime):
-                potentialRecipeIDs.remove(idnum)
-        if (prepTime>0):
-            if (recipe["cookTime"]>prepTime):
-                potentialRecipeIDs.remove(idnum)
-
-
-    # potentialRecipeIDs now full of all recipes with at least one present ingredient, and potRecIngredientRatio is a dict of dicts in correct format
-    def sortAlpha(n):
-        return recipeList[n]["name"]
-    def sortPercent(n):
-        return potRecIngredientRatio[n]["ratio"]
-    def sortPrice(n):
-
-
-
-    if (sortBy == "alpha"):
-        potentialRecipeIDs.sort(key=sortAlpha)
-    elif (sortBy == "price"):
-        potentialRecipeIDs.sort(key=sortPrice)
-    else:
-        potentialRecipeIDs.sort(key=sortPercent)
-
-    # TODO sort dict
-    # TODO: sort by price, alphabetical, percent ingredients
-
-    # this code was written by a human goddamn being with a soul and thoughts.
-
-    return potentialRecipeIDs
