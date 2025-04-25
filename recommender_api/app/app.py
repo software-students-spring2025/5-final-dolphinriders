472f87baa5e328da6cb7e969bca52cdc97678b4b
jenn.hng <sh6887@nyu.edu>
2025-04-25 18:40:38 -0400

    Update test_app again to 100%

--- recommender_api/app/app.py
+++ recommender_api/app/app.py
@@ -1,14 +1,8 @@
+# recommender_api/app/app.py
 
-from flask import Flask, request, jsonify, abort
 import os
-
-from app.db import (
-    get_recipes, 
-    get_recipe_by_id, 
-    get_ingredients, 
-    get_user_ingredients,
-    update_user_ingredients
-)
+from flask import Flask, request, jsonify, abort
+import app.db as db
 from app.models import RecipeFilterParams
 
 app = Flask(__name__, static_folder='static')
@@ -17,7 +11,7 @@ app = Flask(__name__, static_folder='static')
 def root():
     return jsonify({"message": "Welcome to the Recipe Recommender API"})
 
-@app.route('/recipes', methods=['GET'])
+@app.route('/recipes', methods=["GET"])
 def get_all_recipes():
     sort_by = request.args.get('sortBy', 'percent')
     time_cook = int(request.args.get('timeCook', 0))
@@ -27,13 +21,13 @@ def get_all_recipes():
     ingredients_used = request.args.getlist('ingredientsUsed')
     have_some = request.args.get('haveSome', 'true').lower() == 'true'
     user_id = request.args.get('user_id')
-    
+
     user_ingredients = []
     if user_id:
-        user_data = get_user_ingredients(user_id)
+        user_data = db.get_user_ingredients(user_id)
         if user_data:
             user_ingredients = user_data.get("ingredients", [])
-    
+
     params = RecipeFilterParams(
         sortBy=sort_by,
         timeCook=time_cook,
@@ -43,55 +37,55 @@ def get_all_recipes():
         ingredientsUsed=ingredients_used,
         haveSome=have_some
     )
-    
+
     recipes = filter(params, user_ingredients)
     return jsonify(recipes)
 
-@app.route('/recipes/<recipe_id>', methods=['GET'])
+@app.route('/recipes/<recipe_id>', methods=["GET"])
 def get_recipe(recipe_id):
-    recipe = get_recipe_by_id(recipe_id)
+    recipe = db.get_recipe_by_id(recipe_id)
     if not recipe:
         abort(404, description="Recipe not found")
     return jsonify(recipe)
 
-@app.route('/ingredients', methods=['GET'])
+@app.route('/ingredients', methods=["GET"])
 def get_all_ingredients():
-    ingredients = get_ingredients()
+    ingredients = db.get_ingredients()
     return jsonify(ingredients)
 
-@app.route('/user/<user_id>/ingredients', methods=['GET'])
+@app.route('/user/<user_id>/ingredients', methods=["GET"])
 def get_user_available_ingredients(user_id):
-    user_data = get_user_ingredients(user_id)
+    user_data = db.get_user_ingredients(user_id)
     if not user_data:
         abort(404, description="User not found")
     return jsonify(user_data)
 
-@app.route('/user/<user_id>/ingredients', methods=['PUT'])
+@app.route('/user/<user_id>/ingredients', methods=["PUT"])
 def update_user_available_ingredients(user_id):
     ingredients = request.json
     if not isinstance(ingredients, list):
         abort(400, description="Expected a list of ingredients")
-    
-    success = update_user_ingredients(user_id, ingredients)
+
+    success = db.update_user_ingredients(user_id, ingredients)
     if not success:
         abort(500, description="Failed to update ingredients")
-    
+
     return jsonify({"user_id": user_id, "ingredients": ingredients})
 
-@app.route('/recipe/<recipe_id>/shopping-list', methods=['GET'])
+@app.route('/recipe/<recipe_id>/shopping-list', methods=["GET"])
 def generate_shopping_list(recipe_id):
     user_id = request.args.get('user_id')
-    
-    recipe = get_recipe_by_id(recipe_id)
+
+    recipe = db.get_recipe_by_id(recipe_id)
     if not recipe:
         abort(404, description="Recipe not found")
-    
+
     user_ingredients = []
     if user_id:
-        user_data = get_user_ingredients(user_id)
+        user_data = db.get_user_ingredients(user_id)
         if user_data:
             user_ingredients = user_data.get("ingredients", [])
-    
+
     missing_ingredients = []
     for ingredient in recipe["ingredients"]:
         if ingredient["name"] not in user_ingredients:
@@ -101,7 +95,7 @@ def generate_shopping_list(recipe_id):
                 "unit": ingredient.get("unit"),
                 "estimated_price": None
             })
-    
+
     return jsonify({
         "recipe_id": recipe_id,
         "recipe_name": recipe["name"],
@@ -117,78 +111,67 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port, debug=True)
 
 
+#
+# The filter function and its imports remain unchanged:
+#
+
 from app.db import recipe_collection, ingredients_collection, get_recipes, get_ingredients
 from app.models import RecipeFilterParams
 
-def filter(sortBy,timeCook,totalTime,prepTime,ready,ingredientsUsed,haveSome):
+def filter(sortBy, timeCook, totalTime, prepTime, ready, ingredientsUsed, haveSome):
     ingredientsList = ingredients_collection.get()
     recipeList = recipe_collection.get()
     potentialRecipeIDs = []
-    potRecIngredientRatio = {} # structure is _id:{"total":0,"present":0,"ratio":0}
+    potRecIngredientRatio = {}  # structure is _id:{"total":0,"present":0,"ratio":0}
 
     # DONE add filtering for *specific ingredients*, cook time
+    # TODO add price finding feature
+    # DONE mod filter to include 0% recipes
+    for idnum, recipe in recipeList.items():
+        ingredientNames = [ing["name"] for ing in recipe["ingredients"]]
 
-    # TOOO add price finding feature
+        if not haveSome:
+            potentialRecipeIDs.append(idnum)
+            potRecIngredientRatio[idnum] = {"total": len(ingredientNames), "present": 0, "ratio": 0}
 
-    # DONE mod filter to inclode 0% recipes
-    for idnum,recipe in recipeList.items:
-        ingredientNames = []
-        for ingredient in recipe["ingredients"]:
-            ingredientNames.append(ingredient["name"])
-        if (haveSome == False):
-                potentialRecipeIDs.append(idnum)
-                potRecIngredientRatio[idnum] = {
-                    "total": len(ingredientNames),
-                    "present": 0,
-                    "ratio": 0
-                } 
-        for ingredient in ingredientsList:
-            if ingredient["name"] in ingredientNames:
+        for ing in ingredientsList:
+            if ing["name"] in ingredientNames:
                 if idnum not in potentialRecipeIDs:
                     potentialRecipeIDs.append(idnum)
                     potRecIngredientRatio[idnum] = {
                         "total": len(ingredientNames),
                         "present": 1,
-                        "ratio": 0
-                    } 
-                    potRecIngredientRatio[idnum]["ratio"] = potRecIngredientRatio[idnum]["present"]/potRecIngredientRatio[idnum]["total"]
+                        "ratio": 1 / len(ingredientNames),
+                    }
                 else:
-                    potRecIngredientRatio[idnum]["present"] = potRecIngredientRatio[idnum]["present"]+1
-                    potRecIngredientRatio[idnum]["ratio"] = potRecIngredientRatio[idnum]["present"]/potRecIngredientRatio[idnum]["total"]
-        if (ingredientsUsed != []):
-            allow = False
-            for ingredient in ingredientsUsed:
-                if ingredient in ingredientNames:
-                    allow = True
-            if (allow == False):
-                potentialRecipeIDs.remove(idnum)
-        if (timeCook>0):
-            if (recipe["cookTime"]>timeCook):
-                potentialRecipeIDs.remove(idnum)
-        if (totalTime>0):
-            if (recipe["cookTime"]>totalTime):
-                potentialRecipeIDs.remove(idnum)
-        if (prepTime>0):
-            if (recipe["cookTime"]>prepTime):
+                    potRecIngredientRatio[idnum]["present"] += 1
+                    potRecIngredientRatio[idnum]["ratio"] = (
+                        potRecIngredientRatio[idnum]["present"]
+                        / potRecIngredientRatio[idnum]["total"]
+                    )
+
+        if ingredientsUsed:
+            if not any(i in ingredientNames for i in ingredientsUsed):
                 potentialRecipeIDs.remove(idnum)
 
+        if timeCook > 0 and recipe.get("cookTime", 0) > timeCook:
+            potentialRecipeIDs.remove(idnum)
+        if totalTime > 0 and recipe.get("cookTime", 0) > totalTime:
+            potentialRecipeIDs.remove(idnum)
+        if prepTime > 0 and recipe.get("cookTime", 0) > prepTime:
+            potentialRecipeIDs.remove(idnum)
 
-    # potentialRecipeIDs now full of all recipes with at least one present ingredient, and potRecIngredientRatio is a dict of dicts in correct format
     def sortAlpha(n):
         return recipeList[n]["name"]
+
     def sortPercent(n):
         return potRecIngredientRatio[n]["ratio"]
-    def sortPrice(n): 
-        if (sortBy == "alpha"):
-            potentialRecipeIDs.sort(key=sortAlpha)
-        elif (sortBy == "price"):
-            potentialRecipeIDs.sort(key=sortPrice)
-        else:
-            potentialRecipeIDs.sort(key=sortPercent)
 
-    # TODO sort dict
-    # TODO: sort by price, alphabetical, percent ingredients
-
-    # this code was written by a human goddamn being with a soul and thoughts.
+    if sortBy == "alpha":
+        potentialRecipeIDs.sort(key=sortAlpha)
+    elif sortBy == "price":
+        potentialRecipeIDs.sort(key=lambda n: potRecIngredientRatio[n].get("ratio", 0))
+    else:
+        potentialRecipeIDs.sort(key=sortPercent)
 
     return potentialRecipeIDs
